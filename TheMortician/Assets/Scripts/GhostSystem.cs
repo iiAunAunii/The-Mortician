c66d0441aeb65772c4a303df3f5ad8db2ca84cc0
nattapong301 <85746095+nattapong301@users.noreply.github.com>
2022-11-11 21:31:01 +0700

    HP system แต่ยังใช้ไม่ได้

---
+++ TheMortician/Assets/Scripts/GhostSystem.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostSystem : MonoBehaviour
+{
+    public float damage = 20;
+    public float damageTime = 1;
+    public GameObject player;
+    public bool enableDisappear = false;
+    public float timeHealth = 3.0f;
+    IEnumerator damagePlayer = null;
+    IEnumerator healthPlayer = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.tag == "FPSController")
+        {
+            enableDisappear = true;
+            if(healthPlayer != null)          
+            StopCoroutine(healthPlayer);
+
+            damagePlayer = other.GetComponent<HealthSytem>().RemoveHealth(damage,damageTime);
+            StartCoroutine(damagePlayer);
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.transform.tag == "FPSController")
+        {
+            enableDisappear = true;
+            if (healthPlayer != null)
+                StopCoroutine(damagePlayer);
+
+            healthPlayer = other.gameObject.GetComponent<HealthSytem>().StartHealth(other.gameObject.GetComponent<HealthSytem>().health, timeHealth);
+            StartCoroutine(healthPlayer);
+        }
+    }
+}
---
+++ TheMortician/Assets/Scripts/HealthSytem.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSytem : MonoBehaviour
+{
+    public float health = 100f;
+    public float healthMax = 100f;
+    public Slider healthSlider;
+    public GameObject finishPanal;
+
+
+    void Start()
+    {
+        health = healthMax;
+        healthSlider.GetComponent<Slider>().maxValue = healthMax;
+        healthSlider.GetComponent<Slider>().value = health;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSlider.GetComponent<Slider>().value = health;
+    }
+
+
+    public IEnumerator RemoveHealth(float value, float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            if (health > 0)
+            {
+                health -= value;
+            }
+        }
+    }
+
+
+    public IEnumerator StartHealth(float value, float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            if (health > 0 && health < healthMax)
+            {
+                health += value;
+            }
+
+
+            else
+            {
+                health = healthMax;
+            }
+        }
+    }
+}
